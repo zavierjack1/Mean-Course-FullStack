572de4b0beba1181d63201a05938d8f222e5becd
Zavier Jack <zavierjack1@gmail.com>
2020-03-28 14:53:31 -0400

    added dynamic setup to fullstack compose

---
+++ Angular/environment.dynamic.ts
@@ -0,0 +1,4 @@
+export const environment = {  
+    production: false,
+    nodeUrl: 'http://3.21.159.109:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+};
