9a8bb3369e88c4cbba8e62f8015ab9613dfa5b4e
zavier jack <zavierjack1@gmail.com>
2020-03-29 01:09:41 +0000

    using optimation on angular serve

--- Angular/environment.dynamic.ts
+++ Angular/environment.dynamic.ts
@@ -1,4 +1,4 @@
 export const environment = {  
-    production: false,
-    nodeUrl: 'http://3.21.159.109:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+    production: true,
+    nodeUrl: 'http://18.217.66.98:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
 };
