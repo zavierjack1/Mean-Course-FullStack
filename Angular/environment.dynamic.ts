3dce82e1a335aae2ec384525f20a5a1d97428309
Zavier Jack <zavierjack1@gmail.com>
2020-03-29 11:58:26 -0400

    clean up

--- Angular/environment.dynamic.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {  
-    production: true,
-    nodeUrl: 'http://18.217.66.98:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
-};
