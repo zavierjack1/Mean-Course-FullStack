dcf384e1e0c61df312372762d58e7e005dcd1051
zavier jack <zavierjack1@gmail.com>
2020-03-29 16:35:30 +0000

    full stack compose uses mounts to copy project into container

--- Angular/environment.development.ts
+++ Angular/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {  
     production: true,
-    nodeUrl: 'http://18.217.66.98:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+    nodeUrl: 'http://3.135.246.134:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
 };
