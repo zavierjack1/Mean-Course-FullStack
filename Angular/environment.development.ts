c4407a8290f7d43ebf069a7d4970c36be47c15f6
zavier jack <zavierjack1@gmail.com>
2020-03-29 22:08:20 +0000

    added node config

--- Angular/environment.development.ts
+++ Angular/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {  
     production: true,
-    nodeUrl: 'http://3.135.246.134:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
+    nodeUrl: 'http://18.188.110.155:8080' //REMEMBER! this is the IP of NodeExpress Server from perspective of the client. not the angular server
 };
